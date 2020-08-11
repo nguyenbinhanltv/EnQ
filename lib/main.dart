bcd8bf930ed950bb053d0250bc0473b2571ae3b2
linhsol26 <nhglinh.sol@gmail.com>
2020-08-11 14:16:20 +0700

    test background login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/login.dart';
+
 void main() {
   runApp(MyApp());
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,2 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          SvgPicture.asset('assets/background-login.svg',
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height)
+        ],
+      ),
+    );
+  }
+}
