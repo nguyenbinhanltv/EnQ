465a11617a962470998766b47206c6399e21bd7a
linhsol26 <nhglinh.sol@gmail.com>
2020-08-11 15:19:07 +0700

    first step in Login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,13 +9,51 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+    return Container(
+      color: Colors.white,
+      child: Stack(
         children: <Widget>[
-          SvgPicture.asset('assets/background-login.svg',
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 70.0, // will replace later
+              top: 75.0, // will replace later
+            ),
+            child: Text(
+              'EnQ',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+                decoration: TextDecoration.none,
+                fontSize: 60.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 40.0, // will replace later
+              top: 200.0, // will replace later
+              right: 120.0, // will replace later
+            ),
+            child: Text(
+              'Welcome to EnQ, it will help you improve your English',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+                decoration: TextDecoration.none,
+                fontSize: 20.0, // will replace later
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SvgPicture.asset(
+              'assets/background-login.svg',
               alignment: Alignment.center,
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height)
+              // color: Colors.indigo,
+            ),
+          ),
         ],
       ),
     );
