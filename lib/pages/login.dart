284ef8c76f9c79896486edbd868e20d656bba1d8
linhsol26 <nhglinh.sol@gmail.com>
2020-08-18 00:13:43 +0700

    add fonts and test bottom navigation bar

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,8 @@
+import 'package:EnQ/components/login_button.dart';
 import 'package:EnQ/const/size_config.dart';
 import 'package:EnQ/const/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -18,15 +18,16 @@ class _LoginState extends State<Login> {
       child: Stack(
         children: <Widget>[
           Padding(
-              padding: const EdgeInsets.only(
-                right: DefaultPaddin * 7.0,
-                bottom: DefaultPaddin * 25.0,
-              ),
-              child: SvgPicture.asset(
-                'assets/images/logo-noname.svg',
-                width: MediaQuery.of(context).size.width,
-                color: Colors.brown,
-              )),
+            padding: const EdgeInsets.only(
+              right: DefaultPaddin * 7.0,
+              bottom: DefaultPaddin * 25.0,
+            ),
+            child: SvgPicture.asset(
+              'assets/images/logo-noname.svg',
+              width: SizeConfig.screenWidth,
+              color: Colors.brown,
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(
               left: DefaultPaddin * 2.0,
@@ -46,7 +47,7 @@ class _LoginState extends State<Login> {
             child: SvgPicture.asset(
               'assets/images/background-login.svg',
               alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
+              width: SizeConfig.screenWidth,
               // height: MediaQuery.of(context).size.height,
               color: Colors.indigo,
             ),
@@ -67,40 +68,6 @@ class _LoginState extends State<Login> {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class LoginButton extends StatelessWidget {
-  final String _title;
-  final String _imgUrl;
-  LoginButton(this._title, this._imgUrl);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: SizeConfig.screenWidth * 0.7,
-      height: SizeConfig.screenHeight * 0.08,
-      child: FlatButton(
-        onPressed: () {},
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
-            side: BorderSide(color: Colors.black)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SvgPicture.asset(
-              this._imgUrl,
-              width: 24.0,
-              height: 24.0,
-            ),
-            Text(
-              this._title,
-              style: TextStyle(fontSize: SmallText),
-            ),
-            Icon(Icons.arrow_forward),
-          ],
-        ),
       ),
     );
   }
