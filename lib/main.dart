4d46ca78d93a9bcb74042d7d25c80f889024bd56
linhsol26 <nhglinh.sol@gmail.com>
2020-08-28 16:10:54 +0700

    leader board 20%

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         AppRouting.home: (context) => Home(),
         AppRouting.login: (context) => Login(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,6 +3,7 @@ import 'package:EnQ/const/size_config.dart';
 import 'package:EnQ/const/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -17,43 +18,43 @@ class _LoginState extends State<Login> {
       color: Colors.white,
       child: Stack(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(
-              right: DefaultPaddin * 7.0,
-              bottom: DefaultPaddin * 25.0,
-            ),
+          Container(
+            margin: EdgeInsets.only(top: DefaultPaddin * 0.8),
             child: SvgPicture.asset(
               'assets/images/logo-noname.svg',
               width: SizeConfig.screenWidth,
-              color: Colors.brown,
+              color: Colors.black,
+              alignment: Alignment.topRight,
             ),
           ),
           Padding(
             padding: const EdgeInsets.only(
               left: DefaultPaddin * 2.0,
-              top: DefaultPaddin * 12.0,
+              top: DefaultPaddin * 14.0,
               right: DefaultPaddin * 6.0,
             ),
             child: Container(
               margin: EdgeInsets.symmetric(horizontal: DefaultPaddin),
-              child: Text(
-                'Welcome to EnQ, it will help you improve your English',
-                style: ScriptStyle,
+              child: TypewriterAnimatedTextKit(
+                repeatForever: true,
+                isRepeatingAnimation: true,
+                text: ['Welcome to EnQ, it will help you improve your English'],
+                textStyle: ScriptStyle,
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SvgPicture.asset(
-              'assets/images/background-login.svg',
-              alignment: Alignment.center,
-              width: SizeConfig.screenWidth,
-              // height: MediaQuery.of(context).size.height,
-              color: Colors.indigo,
-            ),
-          ),
+          // Align(
+          //   alignment: Alignment.bottomCenter,
+          //   child: SvgPicture.asset(
+          //     'assets/images/background-login.svg',
+          //     alignment: Alignment.center,
+          //     width: SizeConfig.screenWidth,
+          //     // height: MediaQuery.of(context).size.height,
+          //     color: Colors.indigo,
+          //   ),
+          // ),
           Padding(
-            padding: const EdgeInsets.only(top: DefaultPaddin * 12.0),
+            padding: const EdgeInsets.only(top: DefaultPaddin * 15.0),
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
