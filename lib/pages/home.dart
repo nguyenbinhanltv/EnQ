c67fe2f995c77aa0c417699fcdef454a441deb59
duc <hohuynhduca7bh123@gmail.com>
2020-10-20 10:37:29 +0700

    add eva icon

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:EnQ/const/size_config.dart';
 import 'package:EnQ/pages/profile.dart';
 import 'package:EnQ/services/auth_service.dart';
 import 'package:EnQ/utils/app_route.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,72 +11,22 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      switch (_selectedIndex) {
-        case 1:
-          Navigator.of(context).pushNamed(AppRouting.leaderBoard);
-          break;
-        case 2:
-          Navigator.of(context).pushNamed(AppRouting.quiz);
-          break;
-        case 4:
-          Navigator.of(context).pushNamed(AppRouting.profile);
-          break;
-        default:
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
-    AuthService auth = new AuthService();
-
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: SizeConfig.screenWidth,
-          height: SizeConfig.screenHeight,
-          color: Colors.amber,
-          child: FlatButton(
-              onPressed: () => auth.handleSignOut().whenComplete(() =>
-                  Navigator.of(context).popAndPushNamed(AppRouting.login)),
-              child: Text('Sign Out')),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.equalizer),
-            title: Text('Leader'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.view_list),
-            title: Text('Quiz'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            title: Text('History'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text('Profile'),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: <Widget>[
+                Icon(EvaIcons.home),
+                Icon(EvaIcons.barChartOutline),
+                Icon(EvaIcons.listOutline),
+                Icon(EvaIcons.personOutline),
+              ],
+            ),
           ),
         ],
-        currentIndex: 0,
-        selectedItemColor: Colors.black,
-        selectedFontSize: 12,
-        unselectedItemColor: Colors.black,
-        showUnselectedLabels: true,
-        onTap: _onItemTapped,
       ),
     );
   }
