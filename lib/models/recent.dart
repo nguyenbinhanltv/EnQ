a5b7de92abbf1b0114f3c2f148d9866b2e57a698
linhsol26 <nhglinh.sol@gmail.com>
2020-11-05 09:31:31 +0700

    button of recent site in home pre-done

--- lib/models/recent.dart
+++ lib/models/recent.dart
@@ -1,14 +1,12 @@
 class Recent {
-  final String userName, photoUrl, endAt, date, point;
+  final String userName, photoUrl, endAt, date;
+  final int point;
 
   Recent(this.userName, this.photoUrl, this.endAt, this.date, this.point);
-
-  static List<Recent> recents = [
-    Recent("Akita", "assets/images/baby_lion.jpg", "12:30", "Aug 18,2020",
-        "10/10"),
-    Recent("Akita", "assets/images/baby_lion.jpg", "12:30", "Aug 18,2020",
-        "10/10"),
-    Recent("Akita", "assets/images/baby_lion.jpg", "12:30", "Aug 18,2020",
-        "10/10"),
-  ];
 }
+
+List<Recent> recents = [
+  Recent("Akita", "assets/images/baby_lion.jpg", "12:30", "Aug 18,2020", 10),
+  Recent("Akita", "assets/images/baby_lion.jpg", "12:30", "Aug 18,2020", 10),
+  Recent("Akita", "assets/images/baby_lion.jpg", "12:30", "Aug 18,2020", 10),
+];
